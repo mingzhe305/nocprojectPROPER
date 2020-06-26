c7ac15a5e269b60830f09741cad2fec5d5e4b0bb
Wang Mingzhe <wangmingzhe98@hotmail.com>
2020-06-26 16:59:22 +0800

    Project base template!!!
    -try for bright theme etc

---
+++ social_webapp/interestpage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def all_interests(request):
+    return render(request, 'interestpage/all_interests.html')
+
+
+#kiv interest_home
+def interest_home(request, interest_name):
+    return render(request, 'interestpage/interest_home.html')
